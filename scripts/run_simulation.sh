774127f6a823ba665668847e7a2b22d54bae3026
kche309 <8745224+kche309@users.noreply.github.com>
2019-08-05 20:03:11 +1200

    Updated simulation script to create output directories

--- scripts/run_simulation.sh
+++ scripts/run_simulation.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Description: sequence simulation based on SiFit model by Zafar et al. (2017)
 # Usage: ./run_simulation.sh
+mkdir -p output
+mkdir -p output/sequences
+mkdir -p output/xml
+cd r
 
 echo "installing r packages"
 Rscript -e 'install.packages("expm", repos="https://cran.rstudio.com")'
 
 echo "running simulation"
-cd r
 Rscript "simulate_tree.r"
 
 cd ..
