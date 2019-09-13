04a7eb12d6b618ea1150db6c50cb64ecd996328c
kche309 <8745224+kche309@users.noreply.github.com>
2019-09-13 13:31:18 +1200

    Refactored substitution model unit tests

--- test/substitutionmodel/SiFit2Test.java
+++ test/substitutionmodel/SiFit2Test.java
@@ -2,17 +2,25 @@ package substitutionmodel;
 
 import beast.core.Description;
 import beast.core.parameter.RealParameter;
-import junit.framework.TestCase;
+
+import org.junit.*;
+import static org.junit.Assert.*;
 
 @Description("Test SiFit2 matrix exponentiation")
-public class SiFit2Test extends TestCase {
+public class SiFit2Test {
 
-    public interface Instance {
-        Double getLambdaD();
-        Double getLambdaL();
+    private static double DELTA = 1e-10;
 
-        double getDistance();
-        double[] getExpectedResult();
+    private SiFit2 model;
+    private int nrStates;
+
+    public void setupModel(Double lambdaD, Double lambdaL) {
+        model = new SiFit2();
+        model.initByName(
+                "lambdaD", new RealParameter(lambdaD.toString()),
+                "lambdaL", new RealParameter(lambdaL.toString())
+        );
+        nrStates = model.getStateCount();
     }
 
     /**
@@ -37,30 +45,18 @@ public class SiFit2Test extends TestCase {
      * expm(beta * Q * t)
      *
      */
-    protected Instance test0 = new Instance() {
-        @Override
-        public Double getLambdaD() {
-            return 2.0;
-        }
-
-        @Override
-        public Double getLambdaL() {
-            return 3.0;
-        }
-
-        @Override
-        public double getDistance() {
-            return 10;
-        }
-
-        @Override
-        public double[] getExpectedResult() {
-            return new double[] {
-                    0.714285714292, 0.285714285708,
-                    0.714285714269, 0.285714285731
-            };
-        }
-    };
+    @Test
+    public void testTransitionLong() {
+        double t = 10;
+        setupModel(2.0, 3.0);
+        double[] expected = new double[] {
+                0.714285714292, 0.285714285708,
+                0.714285714269, 0.285714285731
+        };
+        double[] observed = new double[nrStates * nrStates];
+        model.getTransitionProbabilities(null, t, 0, 1, observed);
+        assertArrayEquals(expected, observed, DELTA);
+    }
 
     /**
      * results obtained from running the following code in R:
@@ -84,38 +80,25 @@ public class SiFit2Test extends TestCase {
      * expm(beta * Q * t)
      *
      */
-    protected Instance test1 = new Instance() {
-        @Override
-        public Double getLambdaD() {
-            return 2.0;
-        }
-
-        @Override
-        public Double getLambdaL() {
-            return 3.0;
-        }
-
-        @Override
-        public double getDistance() {
-            return 0.1;
-        }
-
-        @Override
-        public double[] getExpectedResult() {
-            return new double[] {
-                    0.937915582355, 0.0620844176452,
-                    0.155211044113, 0.8447889558870
-            };
-        }
-    };
+    @Test
+    public void testTransitionShort() {
+        double t = 0.1;
+        setupModel(2.0, 3.0);
+        double[] expected = new double[] {
+                0.937915582355, 0.0620844176452,
+                0.155211044113, 0.8447889558870
+        };
+        double[] observed = new double[nrStates * nrStates];
+        model.getTransitionProbabilities(null, t, 0, 1, observed);
+        assertArrayEquals(expected, observed, DELTA);
+    }
 
     /**
-     * expect probability to reach equilibrium frequencies when time t is large
      * results obtained from running the following code in R:
      *
      * library(expm)
      * op <- options(digits=12)
-     * t <- 0.1
+     * t <- 10
      * lambdaD <- 2
      * lambdaL <- 3
      * lambdaSum <- lambdaD + lambdaL
@@ -129,63 +112,17 @@ public class SiFit2Test extends TestCase {
      * freq <- c(pi0, pi1)
      * diag <- -diag(Q)
      * beta <- as.vector(1 / (freq %*% diag))
-     * expm(beta * Q * t)
+     * result <- expm(beta * Q * t)
+     * result[1, ]
      *
      */
-    protected Instance test2 = new Instance() {
-        @Override
-        public Double getLambdaD() {
-            return 2.0;
-        }
-
-        @Override
-        public Double getLambdaL() {
-            return 3.0;
-        }
-
-        @Override
-        public double getDistance() {
-            return 10;
-        }
-
-        @Override
-        public double[] getExpectedResult() {
-            SiFit2 model = new SiFit2();
-            model.initByName(
-                    "lambdaD", new RealParameter(getLambdaD().toString()),
-                    "lambdaL", new RealParameter(getLambdaL().toString())
-            );
-            // expect equilibrium frequencies
-            int nrStates = model.getStateCount();
-            double[] freq = model.getFrequencies();
-            double[] result = new double[nrStates * nrStates];
-            for (int i = 0; i < result.length; i++) {
-                result[i] = freq[i % nrStates];
-            }
-            return result;
-        }
-    };
-
-    Instance[] allTests = {test0, test1, test2};
-
-    public void testSiFit2() throws Exception {
-        for (Instance test: allTests) {
-            SiFit2 model = new SiFit2();
-            model.initByName(
-                    "lambdaD", new RealParameter(test.getLambdaD().toString()),
-                    "lambdaL", new RealParameter(test.getLambdaL().toString())
-            );
-
-            int nrStates = model.getStateCount();
-            double distance = test.getDistance();
-            double[] expectedResult = test.getExpectedResult();
-            double[] matrix = new double[nrStates * nrStates];
-
-            model.getTransitionProbabilities(null, distance, 0, 1, matrix);
-            for (int k = 0; k < matrix.length; ++k) {
-                assertEquals(matrix[k], expectedResult[k], 1e-10);
-                System.out.println(k + " : " + (matrix[k] - expectedResult[k]));
-            }
-        }
+    @Test
+    public void testEquilibrium() {
+        setupModel(2.0, 3.0);
+        double[] expected = new double[] {
+                0.714285714292, 0.285714285708
+        };
+        double[] observed = model.getFrequencies();
+        assertArrayEquals(expected, observed, DELTA);
     }
 }
--- test/substitutionmodel/SiFit3Test.java
+++ test/substitutionmodel/SiFit3Test.java
@@ -3,16 +3,25 @@ package substitutionmodel;
 import beast.core.Description;
 import beast.core.parameter.RealParameter;
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 @Description("Test SiFit3 matrix exponentiation")
 public class SiFit3Test extends TestCase {
 
-    public interface Instance {
-        Double getLambdaD();
-        Double getLambdaL();
+    private static double DELTA = 1e-10;
 
-        double getDistance();
-        double[] getExpectedResult();
+    private SiFit3 model;
+    private int nrStates;
+
+    public void setupModel(Double lambdaD, Double lambdaL) {
+        model = new SiFit3();
+        model.initByName(
+                "lambdaD", new RealParameter(lambdaD.toString()),
+                "lambdaL", new RealParameter(lambdaL.toString())
+        );
+        nrStates = model.getStateCount();
     }
 
     /**
@@ -39,31 +48,19 @@ public class SiFit3Test extends TestCase {
      * expm(beta * Q * t)
      *
      */
-    protected Instance test0 = new Instance() {
-        @Override
-        public Double getLambdaD() {
-            return 2.0;
-        }
-
-        @Override
-        public Double getLambdaL() {
-            return 3.0;
-        }
-
-        @Override
-        public double getDistance() {
-            return 10;
-        }
-
-        @Override
-        public double[] getExpectedResult() {
-            return new double[] {
+    @Test
+    public void testTransitionLong() {
+        double t = 10;
+        setupModel(2.0, 3.0);
+        double[] expected = new double[] {
                 0.526735561965, 0.210450674425, 0.262813763610,
                 0.526126686062, 0.210560391486, 0.263312922452,
                 0.525627527220, 0.210650337962, 0.263722134819
-            };
-        }
-    };
+        };
+        double[] observed = new double[nrStates * nrStates];
+        model.getTransitionProbabilities(null, t, 0, 1, observed);
+        assertArrayEquals(expected, observed, DELTA);
+    }
 
     /**
      * results obtained from running the following code in R:
@@ -89,53 +86,17 @@ public class SiFit3Test extends TestCase {
      * expm(beta * Q * t)
      *
      */
-    protected Instance test1 = new Instance() {
-        @Override
-        public Double getLambdaD() {
-            return 2.0;
-        }
-
-        @Override
-        public Double getLambdaL() {
-            return 3.0;
-        }
-
-        @Override
-        public double getDistance() {
-            return 0.1;
-        }
-
-        @Override
-        public double[] getExpectedResult() {
-            return new double[] {
-                    0.95614090897870, 0.0413688413513, 0.00249024967001,
-                    0.10342210337822, 0.7956460429136, 0.10093185370821,
-                    0.00498049934002, 0.0807454829666, 0.91427401769341
-            };
-        }
-    };
-
-    Instance[] allTests = {test0, test1};
-
-    public void  testSiFit3() throws Exception {
-        for (Instance test: allTests) {
-            SiFit3 model = new SiFit3();
-            model.initByName(
-                    "lambdaD", new RealParameter(test.getLambdaD().toString()),
-                    "lambdaL", new RealParameter(test.getLambdaL().toString())
-            );
-
-            int nrStates = model.getStateCount();
-            double distance = test.getDistance();
-            double[] expectedResult = test.getExpectedResult();
-            double[] matrix = new double[nrStates * nrStates];
-
-            model.getTransitionProbabilities(null, distance, 0, 1, matrix);
-            for (int k = 0; k < matrix.length; ++k) {
-                assertEquals(matrix[k], expectedResult[k], 1e-10);
-                System.out.println(k + " : " + (matrix[k] - expectedResult[k]));
-            }
-        }
+    @Test
+    public void testTransitionShort() {
+        double t = 0.1;
+        setupModel(2.0, 3.0);
+        double[] expected = new double[] {
+                0.95614090897870, 0.0413688413513, 0.00249024967001,
+                0.10342210337822, 0.7956460429136, 0.10093185370821,
+                0.00498049934002, 0.0807454829666, 0.91427401769341
+        };
+        double[] observed = new double[nrStates * nrStates];
+        model.getTransitionProbabilities(null, t, 0, 1, observed);
+        assertArrayEquals(expected, observed, DELTA);
     }
-
 }
