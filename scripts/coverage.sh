15ce51a68660b790cfe75e3da3a4b7b7cd5fd465
kche309 <8745224+kche309@users.noreply.github.com>
2019-10-15 14:19:46 +1300

    Updated error model and coverage script

---
+++ scripts/coverage.sh
@@ -0,0 +1,44 @@
+# install screen
+sudo apt-get update
+sudo apt-get install screen
+# install parallel
+sudo apt-get install parallel
+# install samtools
+mkdir ~/repos
+cd ~/repos
+git clone https://github.com/samtools/samtools.git
+git clone git://github.com/samtools/htslib.git
+git clone git://github.com/samtools/bcftools.git
+sudo apt-get update
+sudo apt-get install autoconf automake make gcc perl zlib1g-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libssl-dev libncurses5-dev
+autoheader
+autoconf -Wno-syntax
+./configure
+make
+make install
+# setup volume vdc for data
+# data storage mounted at /pvol
+sudo mkfs -t ext4 /dev/vdc
+sudo mkdir /pvol
+sudo mount /dev/vdc /pvol -t auto
+dufo mount --all
+sudo chown ubuntu:ubuntu /pvol
+# copy data to server
+# example destination: ubuntu@123.123.255.255:/pvol
+scp -rp <src> <destination>
+# setup volume vde for results
+# result storage mounted at /evol
+sudo mkfs -t ext4 /dev/vde
+sudo mkdir /evol
+sudo mount /dev/vde /evol -t auto
+dufo mount --all
+sudo chown ubuntu:ubuntu /evol
+# setup directories
+mkdir /evol/pvol
+mkdir /evol/pvol/Tamsin
+mkdir /evol/pvol/Tamsin/BAM
+# launch screen and run samtools
+ls /pvol/Tamsin/BAM/*.bam | parallel "samtools depth {} > /evol{}.txt" 
+# count number of sites per depth
+# output: number of sites, depth
+ls /evol/pvol/Tamsin/BAM/*.txt | parallel "cat {} | awk '{print $3}' | sort -n | uniq -c > {}.count"
