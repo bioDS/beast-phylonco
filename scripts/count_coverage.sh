48109a2b69caba4fb582bc3cd035ca96296dbce2
kche309 <8745224+kche309@users.noreply.github.com>
2019-11-07 01:55:13 +1300

    Removed files not required for core package.

--- scripts/count_coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# counts number of sites per read depth
-# usage: ./count_coverage.sh <path>
-# output: number of sites, depth
-for file in $1
-do
-	echo "processing file: $file"
-	cat $file | awk '{print $3}' | sort -n | uniq -c > "$file.count"
-done
