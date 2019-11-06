c5a7f685b4beef646b076ec304d43ba14be76194
Jiří Moravec <jiri.c.moravec@gmail.com>
2019-11-06 16:29:59 +1300

    Copied getTaxonID function from TreeLikelihood
    Correct approach seems to be to copy getTaxonID from TreeLikelihood, instead of use it directly? BeagleTreeLikelihood use this approach and setting it to protected or public doesn't work.

--- src/beast/evolution/likelihood/TreeLikelihoodWithError.java
+++ src/beast/evolution/likelihood/TreeLikelihoodWithError.java
@@ -3,7 +3,6 @@ package beast.evolution.likelihood;
 import beast.core.Input;
 import beast.evolution.alignment.Alignment;
 import beast.evolution.datatype.DataTypeWithError;
-import beast.evolution.likelihood.TreeLikelihood;
 import beast.evolution.tree.Node;
 
 public class TreeLikelihoodWithError extends TreeLikelihood {
@@ -24,6 +23,28 @@ public class TreeLikelihoodWithError extends TreeLikelihood {
         super.m_useTipLikelihoods.set(true);
         super.initAndValidate();
     }
+
+
+    /**
+     *
+     * @param taxon the taxon name as a string
+     * @param data the alignment
+     * @return the taxon index of the given taxon name for accessing its sequence data in the given alignment,
+     *         or -1 if the taxon is not in the alignment.
+     */
+    private int getTaxonIndex(String taxon, Alignment data) {
+        int taxonIndex = data.getTaxonIndex(taxon);
+        if (taxonIndex == -1) {
+            if (taxon.startsWith("'") || taxon.startsWith("\"")) {
+                taxonIndex = data.getTaxonIndex(taxon.substring(1, taxon.length() - 1));
+            }
+            if (taxonIndex == -1) {
+                throw new RuntimeException("Could not find sequence " + taxon + " in the alignment");
+            }
+        }
+        return taxonIndex;
+    }
+
 
     @Override
     protected void setPartials(Node node, int nrOfPatterns) {
