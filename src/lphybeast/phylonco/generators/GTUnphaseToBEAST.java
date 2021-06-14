30414eb1dd9dfdf73a80f3302a2f9bf2352858e2
Walter <walterxie@users.noreply.github.com>
2021-06-15 11:45:32 +1200

    init dev of the plugin functionality for LPhyBEAST LinguaPhylo/LPhyBeast#40

---
+++ src/lphybeast/phylonco/generators/GTUnphaseToBEAST.java
@@ -0,0 +1,71 @@
+package lphybeast.phylonco.generators;
+
+import beast.core.BEASTInterface;
+import beast.evolution.likelihood.TreeLikelihoodWithError;
+import lphy.evolution.alignment.Alignment;
+import lphy.evolution.functions.UnphaseGenotypeAlignment;
+import lphy.graphicalModel.Generator;
+import lphy.graphicalModel.GraphicalModelNode;
+import lphy.graphicalModel.Value;
+import lphybeast.BEASTContext;
+import lphybeast.GeneratorToBEAST;
+
+import java.util.List;
+
+/**
+ * This has to create TreeLikelihood.
+ * A ~ PhyloCTMC(); E ~ ErrorModel(A); D = unphase(E);
+ * @author Walter Xie
+ */
+public class GTUnphaseToBEAST implements GeneratorToBEAST<UnphaseGenotypeAlignment, TreeLikelihoodWithError>  {
+
+    @Override
+    public TreeLikelihoodWithError generatorToBEAST(UnphaseGenotypeAlignment generator, BEASTInterface value, BEASTContext context) {
+
+        assert value instanceof beast.evolution.alignment.Alignment;
+        beast.evolution.alignment.Alignment unphasedErrAlignment = (beast.evolution.alignment.Alignment)value;
+
+        Value<Alignment> errAlignmentInput = null;
+        Generator<?>  errAligGenerator = null;
+        List<GraphicalModelNode<?>> inputs = generator.getInputs();
+        for (GraphicalModelNode<?> input : inputs) {
+            if (input instanceof Value && input.value() instanceof Alignment) {
+                errAlignmentInput = (Value<Alignment>) input;
+                errAligGenerator = errAlignmentInput.getGenerator();
+                break;
+            }
+        }
+
+        // TreeLikelihoodWithError should be already created for err alignment
+        TreeLikelihoodWithError treeLikelihoodWithError =
+                (TreeLikelihoodWithError) context.getBEASTObject(errAligGenerator);
+
+        if (treeLikelihoodWithError == null)
+            throw new IllegalArgumentException("Cannot find err alignment tree likelihood !");
+
+        treeLikelihoodWithError.setInputValue("data", unphasedErrAlignment);
+
+        treeLikelihoodWithError.initAndValidate();
+        treeLikelihoodWithError.setID(unphasedErrAlignment.getID() + ".treeLikelihood");
+
+        BEASTInterface errAlignment = context.getBEASTObject(errAlignmentInput);
+        context.removeBEASTObject(errAlignment);
+        // remove previous treeLikelihoodWithError added by GT16ErrorModelToBEAST
+        context.removeBEASTObject(treeLikelihoodWithError);
+
+        // logging
+        context.addExtraLogger(treeLikelihoodWithError);
+        return treeLikelihoodWithError;
+    }
+
+
+    @Override
+    public Class<UnphaseGenotypeAlignment> getGeneratorClass() {
+        return UnphaseGenotypeAlignment.class;
+    }
+
+    @Override
+    public Class<TreeLikelihoodWithError> getBEASTClass() {
+        return TreeLikelihoodWithError.class;
+    }
+}
---
+++ src/lphybeast/registry/PhyloncoRegistry.java
@@ -0,0 +1,41 @@
+package lphybeast.registry;
+
+import beast.evolution.datatype.DataType;
+import jebl.evolution.sequences.SequenceType;
+import lphy.evolution.datatype.PhasedGenotype;
+import lphybeast.phylonco.generators.GT16ErrorModelToBEAST;
+import lphybeast.phylonco.generators.GT16ToBEAST;
+import lphybeast.phylonco.generators.GTUnphaseToBEAST;
+import lphybeast.registry.ClassesRegistry;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Walter Xie
+ */
+public class PhyloncoRegistry implements ClassesRegistry {
+
+    private final Class[] generatorToBEASTs = {
+            GT16ErrorModelToBEAST.class,
+            GT16ToBEAST.class,
+            GTUnphaseToBEAST.class
+    };
+
+    @Override
+    public Class[] getValuesToBEASTs() {
+        return new Class[0];
+    }
+
+    @Override
+    public Class[] getGeneratorToBEASTs() {
+        return generatorToBEASTs;
+    }
+
+    @Override
+    public Map<SequenceType, DataType> getDataTypeMap() {
+        Map<SequenceType, DataType> dataTypeMap = new ConcurrentHashMap<>();
+        dataTypeMap.put(PhasedGenotype.INSTANCE, new beast.evolution.datatype.NucleotideDiploid16());
+        return dataTypeMap;
+    }
+}
