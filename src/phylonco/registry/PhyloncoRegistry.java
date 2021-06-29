18e3bdbbdf985e5ed566c63f3252c52e5b59d3f3
Walter <walterxie@users.noreply.github.com>
2021-06-29 16:23:05 +1200

    allow to extend excluded generators and values #2

--- src/phylonco/registry/PhyloncoRegistry.java
+++ src/phylonco/registry/PhyloncoRegistry.java
@@ -3,12 +3,20 @@ package phylonco.registry;
 import beast.evolution.datatype.DataType;
 import jebl.evolution.sequences.SequenceType;
 import lphy.core.LPhyParser;
-import phylonco.evolution.datatype.PhasedGenotype;
+import lphy.graphicalModel.Generator;
+import lphy.graphicalModel.Value;
+import lphybeast.GeneratorToBEAST;
+import lphybeast.ValueToBEAST;
 import lphybeast.registry.ClassesRegistry;
+import phylonco.evolution.datatype.PhasedGenotype;
+import phylonco.evolution.datatype.PhasedGenotypeFunction;
 import phylonco.tobeast.generators.GT16ErrorModelToBEAST;
 import phylonco.tobeast.generators.GT16ToBEAST;
 import phylonco.tobeast.generators.GTUnphaseToBEAST;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,20 +28,15 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class PhyloncoRegistry implements ClassesRegistry {
 
-    private final Class<?>[] generatorToBEASTs = {
-            GT16ErrorModelToBEAST.class,
-            GT16ToBEAST.class,
-            GTUnphaseToBEAST.class
-    };
-
     @Override
-    public Class<?>[] getValuesToBEASTs() {
-        return new Class[0];
+    public List<Class<? extends ValueToBEAST>> getValuesToBEASTs() {
+        return Arrays.asList(new Class[0]);
     }
 
     @Override
-    public Class<?>[] getGeneratorToBEASTs() {
-        return generatorToBEASTs;
+    public List<Class<? extends GeneratorToBEAST>> getGeneratorToBEASTs() {
+        return Arrays.asList( GT16ErrorModelToBEAST.class,
+                GT16ToBEAST.class, GTUnphaseToBEAST.class );
     }
 
     @Override
@@ -42,4 +45,16 @@ public class PhyloncoRegistry implements ClassesRegistry {
         dataTypeMap.put(PhasedGenotype.INSTANCE, new beast.evolution.datatype.NucleotideDiploid16());
         return dataTypeMap;
     }
+
+    @Override
+    public List<Class<? extends Generator>> getExcludedGenerator() {
+        return Arrays.asList(PhasedGenotypeFunction.class);
+    }
+
+    @Override
+    public List<Class<? extends Value>> getExcludedValue() {
+        return new ArrayList<>();
+    }
+
+
 }
