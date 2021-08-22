d266b7bc3cb5b72bf914e2901dc5269cb54b5b9b
kche309 <8745224+kche309@users.noreply.github.com>
2021-08-23 00:31:48 +1200

    Added extra operators for lphy to beast generation

--- src/phylonco/lphybeast/tobeast/generators/GT16ToBEAST.java
+++ src/phylonco/lphybeast/tobeast/generators/GT16ToBEAST.java
@@ -1,11 +1,16 @@
 package phylonco.lphybeast.tobeast.generators;
 
 import beast.core.BEASTInterface;
+import beast.core.parameter.BooleanParameter;
+import beast.core.parameter.IntegerParameter;
 import beast.core.parameter.RealParameter;
+import beast.evolution.operators.DeltaExchangeOperator;
+import beast.evolution.operators.SwapOperator;
 import beast.evolution.substitutionmodel.Frequencies;
 import lphy.graphicalModel.Value;
 import lphybeast.BEASTContext;
 import lphybeast.GeneratorToBEAST;
+import org.apache.commons.lang3.ArrayUtils;
 import phylonco.lphy.evolution.substitutionmodel.GT16;
 
 public class GT16ToBEAST implements GeneratorToBEAST<GT16, phylonco.beast.evolution.substitutionmodel.GT16> {
@@ -31,7 +36,69 @@ public class GT16ToBEAST implements GeneratorToBEAST<GT16, phylonco.beast.evolut
         beastGT16.setInputValue("frequencies", freqsParameter);
         beastGT16.initAndValidate();
 
+        // add extra operators
+        addExtraDeltaExchangeOperators(freqsParameter, context);
+        addExtraSwapOperators(freqsParameter, context);
+
         return beastGT16;
+    }
+
+    private void addExtraDeltaExchangeOperators(Frequencies freqsParameter, BEASTContext context) {
+        // heterozygous pairs
+        String[] filterArray = {
+                "0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0",
+                "0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0",
+                "0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0",
+                "0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0",
+                "0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0",
+                "0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0"
+        };
+
+        IntegerParameter[] pairs = new IntegerParameter[filterArray.length];
+        for (int i = 0; i < filterArray.length; i++) {
+            String filter = filterArray[i];
+            pairs[i] = new IntegerParameter(filter);
+        };
+
+        for (int i = 0; i < pairs.length; i++) {
+            DeltaExchangeOperator operator = new DeltaExchangeOperator();
+            operator.setInputValue("parameter", freqsParameter.frequenciesInput.get());
+            operator.setInputValue("autoOptimize", false);
+            operator.setInputValue("delta", 1.0 / 16);
+            operator.setInputValue("weightvector", pairs[i]);
+            operator.setInputValue("weight", "1.0"); // set operator weight to 1
+            operator.setID("deltaExchangePair" + (i + 1));
+            operator.initAndValidate();
+            context.addExtraOperator(operator);
+        }
+    }
+
+    private void addExtraSwapOperators(Frequencies freqsParameter, BEASTContext context) {
+        // heterozygous pairs
+        String[] filterArray = {
+            "0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0",
+            "0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0",
+            "0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0",
+            "0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0",
+            "0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0",
+            "0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0"
+        };
+
+        BooleanParameter[] pairs = new BooleanParameter[filterArray.length];
+        for (int i = 0; i < filterArray.length; i++) {
+            String filter = filterArray[i];
+            pairs[i] = new BooleanParameter(filter);
+        };
+
+        for (int i = 0; i < pairs.length; i++) {
+            SwapOperator operator = new SwapOperator();
+            operator.setInputValue("parameter", freqsParameter.frequenciesInput.get());
+            operator.setInputValue("filter", pairs[i]);
+            operator.setInputValue("weight", "0.5"); // set operator weight to 0.5
+            operator.setID("swapPair" + (i + 1));
+            operator.initAndValidate();
+            context.addExtraOperator(operator);
+        }
     }
 
     @Override
