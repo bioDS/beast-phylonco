968269c854c55b0b0f437602f5c9e3c7794ce447
EvaLiyt <liyutong0617@gmail.com>
2024-01-23 14:58:20 +1300

    HaploidType method

--- phylonco-lphy/src/main/java/phylonco/lphy/spi/PhyloncoImpl.java
+++ phylonco-lphy/src/main/java/phylonco/lphy/spi/PhyloncoImpl.java
@@ -4,9 +4,11 @@ import lphy.base.spi.LPhyBaseImpl;
 import lphy.core.model.BasicFunction;
 import lphy.core.model.GenerativeDistribution;
 import phylonco.lphy.evolution.alignment.GT16ErrorModel;
+import phylonco.lphy.evolution.alignment.HaploidAlignment;
 import phylonco.lphy.evolution.alignment.UnphaseGenotypeAlignment;
 import phylonco.lphy.evolution.datatype.PhasedGenotypeFunction;
 import phylonco.lphy.evolution.substitutionmodel.GT16;
+import phylonco.lphy.io.WriteHaploidFasta;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,12 +30,12 @@ public class PhyloncoImpl extends LPhyBaseImpl {
 
     @Override
     public List<Class<? extends GenerativeDistribution>> declareDistributions() {
-        return Arrays.asList( GT16ErrorModel.class );
+        return Arrays.asList( GT16ErrorModel.class);
     }
 
     @Override
     public List<Class<? extends BasicFunction>> declareFunctions() {
-        return Arrays.asList( GT16.class, PhasedGenotypeFunction.class, UnphaseGenotypeAlignment.class);
+        return Arrays.asList( GT16.class, PhasedGenotypeFunction.class, UnphaseGenotypeAlignment.class, WriteHaploidFasta.class);
     }
 
     public String getExtensionName() {
