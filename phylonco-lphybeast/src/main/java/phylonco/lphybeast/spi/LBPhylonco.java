71528aa7dd988f996e19f954f205e8957ebe9ddb
EvaLiyt <liyutong0617@gmail.com>
2024-07-08 15:18:38 +1200

    implement LocalClockToBeast #62

--- phylonco-lphybeast/src/main/java/phylonco/lphybeast/spi/LBPhylonco.java
+++ phylonco-lphybeast/src/main/java/phylonco/lphybeast/spi/LBPhylonco.java
@@ -2,23 +2,23 @@ package phylonco.lphybeast.spi;
 
 import beast.base.evolution.datatype.DataType;
 import jebl.evolution.sequences.SequenceType;
-import lphy.base.evolution.branchrate.LocalClock;
 import lphy.base.evolution.coalescent.PopulationFunctionCoalescent;
 import lphy.base.evolution.coalescent.populationmodel.*;
-import lphy.base.evolution.tree.TimeTreeNode;
+import lphy.base.evolution.tree.*;
+import lphy.base.function.Difference;
+import lphy.base.function.Union;
+import lphy.base.function.io.ReadTrees;
 import lphy.core.model.Generator;
 import lphybeast.GeneratorToBEAST;
 import lphybeast.ValueToBEAST;
 import lphybeast.spi.LPhyBEASTExt;
+import lphybeast.tobeast.generators.CalibratedYuleToBeast;
 import phylonco.beast.evolution.datatype.NucleotideDiploid16;
 import phylonco.lphy.evolution.alignment.HaploidAlignment;
 import phylonco.lphy.evolution.alignment.HomozygousAlignmentDistribution;
 import phylonco.lphy.evolution.datatype.PhasedGenotype;
 import phylonco.lphy.evolution.datatype.PhasedGenotypeFunction;
-import phylonco.lphybeast.tobeast.generators.GT16ErrorModelToBEAST;
-import phylonco.lphybeast.tobeast.generators.GT16ToBEAST;
-import phylonco.lphybeast.tobeast.generators.GTUnphaseToBEAST;
-import phylonco.lphybeast.tobeast.generators.PopFuncCoalescentToBEAST;
+import phylonco.lphybeast.tobeast.generators.*;
 import phylonco.lphybeast.tobeast.values.*;
 
 import java.util.Arrays;
@@ -47,7 +47,8 @@ public class LBPhylonco implements LPhyBEASTExt {
     public List<Class<? extends GeneratorToBEAST>> getGeneratorToBEASTs() {
         return Arrays.asList( GT16ErrorModelToBEAST.class,
                 GT16ToBEAST.class, GTUnphaseToBEAST.class,
-                PopFuncCoalescentToBEAST.class//, GompertzToBEAST.class
+                PopFuncCoalescentToBEAST.class,
+                LocalClockToBeast.class, CalibratedYuleToBeast.class//, GompertzToBEAST.class
 //                , LogisticToBEAST.class, PopulationFunctionCoalescentToBEAST.class
         );
     }
@@ -62,8 +63,8 @@ public class LBPhylonco implements LPhyBEASTExt {
     @Override
     public List<Class<? extends Generator>> getExcludedGenerator() {
         return Arrays.asList(PhasedGenotypeFunction.class, HomozygousAlignmentDistribution.class,
-                HaploidAlignment.class,
-                LocalClock.class, //TODO
+                HaploidAlignment.class, Difference.class, Union.class, ReadTrees.class,
+                SampleBranch.class, SubstituteClade.class, SubsampledTree.class, LabelClade.class,
                 PopulationFunctionCoalescent.class,
                 ConstantPopulationFunction.class, GompertzPopulationFunction_f0.class, GompertzPopulationFunction_t50.class, ExponentialPopulationFunction.class, LogisticPopulationFunction.class);
     }
