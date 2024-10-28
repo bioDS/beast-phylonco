2ec6f5bd92cdf7d4ccd13ebb2ba71b86168ab642
zjzxiaohei <108013625+zjzxiaohei@users.noreply.github.com>
2024-10-28 14:26:40 +1300

    Fix a bug(getBEASTObject for RealParam) #56

--- phylonco-lphybeast/src/main/java/phylonco/lphybeast/tobeast/generators/ReadCountModelToBEAST.java
+++ phylonco-lphybeast/src/main/java/phylonco/lphybeast/tobeast/generators/ReadCountModelToBEAST.java
@@ -1,6 +1,7 @@
 package phylonco.lphybeast.tobeast.generators;
 
 import beast.base.core.BEASTInterface;
+import beast.base.inference.parameter.RealParameter;
 import lphy.core.model.Value;
 import lphybeast.BEASTContext;
 import lphybeast.GeneratorToBEAST;
@@ -13,21 +14,37 @@ public class ReadCountModelToBEAST implements GeneratorToBEAST<ReadCountModel, L
     public LikelihoodReadCountModel generatorToBEAST(ReadCountModel generator, BEASTInterface value, BEASTContext context) {
         LikelihoodReadCountModel likelihoodReadCountModel = new LikelihoodReadCountModel();
         //Get value from LPhy
-        Value epsilonValue = generator.getParams().get("epsilon");
-        Value deltaValue = (Value) generator.getParams().get("alpha").getGenerator().getParams().get("delta");
-        Value tValue = (Value) generator.getParams().get("coverage").getGenerator().getParams().get("t");
-        Value vValue = (Value) generator.getParams().get("coverage").getGenerator().getParams().get("v");
-        Value sValue = (Value) generator.getParams().get("coverage").getGenerator().getParams().get("s");
-        Value wValue = generator.getParams().get("w");
-        Value alignmentValue = generator.getParams().get("D");
+        String epsilonParamName = "epsilon";
+        String alphaParamName = "alpha";
+        String deltaParamName = "delta";
+        String covParamName = "coverage";
+        String tParamName = "t";
+        String vParamName = "v";
+        String sParamName = "s";
+        String wParamName = "w";
+        String alignmentParamName = "D";
 
-        BEASTInterface epsilonParam = context.getBEASTObject(epsilonValue);
-        BEASTInterface deltaParam = context.getBEASTObject(deltaValue);
-        BEASTInterface tParam = context.getBEASTObject(tValue);
-        BEASTInterface vParam = context.getBEASTObject(vValue);
-        BEASTInterface sParam = context.getBEASTObject(sValue);
-        BEASTInterface wParam = context.getBEASTObject(wValue);
+        Value epsilonValue = generator.getParams().get(epsilonParamName);
+        Value alphaValue = generator.getParams().get(alphaParamName);
+        Value deltaValue = (Value) alphaValue.getGenerator().getParams().get(deltaParamName);
+        Value coverageValue = generator.getParams().get(covParamName);
+        Value tValue = (Value) coverageValue.getGenerator().getParams().get(tParamName);
+        Value vValue = (Value) coverageValue.getGenerator().getParams().get(vParamName);
+        Value sValue = (Value) coverageValue.getGenerator().getParams().get(sParamName);
+        Value wValue = generator.getParams().get(wParamName);
+        Value alignmentValue = generator.getParams().get(alignmentParamName);
+
+
+        RealParameter epsilonParam = context.getAsRealParameter(epsilonValue);
+        RealParameter deltaParam = context.getAsRealParameter(deltaValue);
+        RealParameter tParam = context.getAsRealParameter(tValue);
+        RealParameter vParam = context.getAsRealParameter(vValue);
+        RealParameter sParam = context.getAsRealParameter(sValue);
+        RealParameter wParam = context.getAsRealParameter(wValue);
         BEASTInterface alignmentParam = context.getBEASTObject(alignmentValue);
+        if (alignmentParam == null) {
+            alignmentParam = context.getBEASTObject(alignmentValue.getId());
+        }
 
         likelihoodReadCountModel.setInputValue("epsilon", epsilonParam);
         likelihoodReadCountModel.setInputValue("delta", deltaParam);
