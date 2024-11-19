10ea9498df6d7e18beb18a8b9ff0b8494ff5a9f6
yxu927 <132981793+yxu927@users.noreply.github.com>
2024-11-20 12:08:04 +1300

    change comments

--- phylonco-lphybeast/src/main/java/phylonco/lphybeast/tobeast/values/ExponentialToBEAST.java
+++ phylonco-lphybeast/src/main/java/phylonco/lphybeast/tobeast/values/ExponentialToBEAST.java
@@ -16,32 +16,32 @@ public class ExponentialToBEAST implements ValueToBEAST<ExponentialPopulation, E
 
         ExponentialGrowth beastPopFunc;
 
-        // 获取 ExponentialPopulationFunction 的实例
+
         ExponentialPopulationFunction gen = (ExponentialPopulationFunction) lphyPopFuncVal.getGenerator();
 
-        // 获取 GrowthRate 和 N0 参数
+
         RealParameter GrowthRateParam = context.getAsRealParameter(gen.getGrowthRate());
         RealParameter N0Param = context.getAsRealParameter(gen.getN0());
 
         beastPopFunc = new ExponentialGrowth();
 
-        // 设置 GrowthRate 和 N0 输入
+
         beastPopFunc.setInputValue("GrowthRate", GrowthRateParam);
         beastPopFunc.setInputValue("N0", N0Param);
 
-        // 检查是否包含 NA 参数
+
         Value<Double> naValue = gen.getNA();
         if (naValue != null && naValue.value() != null && naValue.value() > 0.0) {
-            // 获取 NA 参数
+
             RealParameter NAParam = context.getAsRealParameter(gen.getNA());
-            // 设置 NA 输入
+
             beastPopFunc.setInputValue("NA", NAParam);
         }
 
-        // 初始化并验证 BEAST 模型
+
         beastPopFunc.initAndValidate();
 
-        // 设置参数 ID
+
         ValueToParameter.setID(beastPopFunc, lphyPopFuncVal);
 
         return beastPopFunc;
