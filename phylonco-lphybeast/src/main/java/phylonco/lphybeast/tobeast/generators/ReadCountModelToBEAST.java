09eeae9580b31955893bcc62212aa94f17496fc3
zjzxiaohei <108013625+zjzxiaohei@users.noreply.github.com>
2025-02-25 16:46:22 +1300

    Change the datatype of negr for int to double #56

--- phylonco-lphybeast/src/main/java/phylonco/lphybeast/tobeast/generators/ReadCountModelToBEAST.java
+++ phylonco-lphybeast/src/main/java/phylonco/lphybeast/tobeast/generators/ReadCountModelToBEAST.java
@@ -1,13 +1,22 @@
 package phylonco.lphybeast.tobeast.generators;
 
 import beast.base.core.BEASTInterface;
+import beast.base.core.Function;
+import beast.base.evolution.operator.kernel.AdaptableVarianceMultivariateNormalOperator;
+import beast.base.inference.StateNode;
+import beast.base.inference.operator.UpDownOperator;
+import beast.base.inference.operator.kernel.Transform;
 import beast.base.inference.parameter.RealParameter;
 import lphy.core.model.Value;
 import lphybeast.BEASTContext;
 import lphybeast.GeneratorToBEAST;
 import phylonco.beast.evolution.datatype.ReadCount;
 import phylonco.beast.evolution.readcountmodel.LikelihoodReadCountModel;
+import phylonco.beast.evolution.readcountmodel.MutableAlignmentOperator;
 import phylonco.lphy.evolution.readcountmodel.ReadCountModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadCountModelToBEAST implements GeneratorToBEAST<ReadCountModel, LikelihoodReadCountModel> {
     @Override
@@ -62,9 +71,109 @@ public class ReadCountModelToBEAST implements GeneratorToBEAST<ReadCountModel, L
         }
 
         likelihoodReadCountModel.initAndValidate();
+        List<Transform> transforms = new ArrayList<>();
+        //List<Function> logitFunctions = new ArrayList<>();
+        List<Function> logFunctions = new ArrayList<>();
+        //logitFunctions.add(deltaParam);
+        //logitFunctions.add(epsilonParam);
+        //logFunctions.add(sParam);
+        logFunctions.add(tParam);
+        logFunctions.add(vParam);
+        //logFunctions.add(wParam);
+
+        //transforms.add(addLogitTransform(logitFunctions));
+        transforms.add(addLogTransform(logFunctions));
+
+        addAVMNOperator(context, transforms);
+
+        List<StateNode> upStates = new ArrayList<>();
+        //upStates.add(sParam);
+        List<StateNode> downStates = new ArrayList<>();
+        downStates.add(tParam);
+        downStates.add(vParam);
+        addUpDownOperator(context, upStates, downStates);
+        //addMutableAlignmentOperator(context, alignmentParam);
+
+        //context.addSkipOperator(sParam);
+//        context.addSkipOperator(vParam);
+//        context.addSkipOperator(tParam);
+//        context.addSkipOperator(wParam);
+//        context.addSkipOperator(deltaParam);
+//        context.addSkipOperator(epsilonParam);
 
         return likelihoodReadCountModel;
     }
+
+
+
+    private Transform addLogTransform(List<Function> functions) {
+
+        Transform.LogTransform logTransform = new Transform.LogTransform();
+        logTransform.setInputValue("f", functions);
+        logTransform.initAndValidate();
+        return logTransform;
+    }
+
+    private Transform addNoTransform(List<Function> functions) {
+
+        Transform.NoTransform noTransform = new Transform.NoTransform();
+        noTransform.setInputValue("f", functions);
+        noTransform.initAndValidate();
+        return noTransform;
+    }
+
+    private Transform addLogitTransform(List<Function> functions) {
+
+        Transform.LogitTransform logitTransform = new Transform.LogitTransform();
+        logitTransform.setInputValue("f", functions);
+        logitTransform.initAndValidate();
+        return logitTransform;
+    }
+
+    private void addAVMNOperator(BEASTContext context, List<Transform> transforms) {
+        AdaptableVarianceMultivariateNormalOperator operator = new AdaptableVarianceMultivariateNormalOperator();
+
+
+        operator.setInputValue("weight", 8.0);
+        operator.setInputValue("coefficient", 1.0);
+        operator.setInputValue("scaleFactor", 1.0);
+        operator.setInputValue("beta", 0.05);
+        operator.setInputValue("initial", 200 * transforms.size());
+        operator.setInputValue("burnin", 100 * transforms.size());
+        operator.setInputValue("every", 1);
+        operator.setInputValue("allowNonsense", false);
+        operator.setInputValue("transformations", transforms);
+        operator.initAndValidate();
+        operator.setID("AVMN");
+
+        // add operator
+        context.addExtraOperator(operator);
+        // skip default operator schedule
+
+    }
+
+    private void addUpDownOperator(BEASTContext context, List<StateNode> upStateNode, List<StateNode> downStateNode) {
+        UpDownOperator operator = new UpDownOperator();
+        operator.setInputValue("up", upStateNode);
+        operator.setInputValue("down", downStateNode);
+        operator.setInputValue("weight", 3.0);
+        operator.setInputValue("scaleFactor", 0.75);
+
+
+        operator.initAndValidate();
+        operator.setID("upDownOperator");
+        context.addExtraOperator(operator);
+    }
+
+    private void addMutableAlignmentOperator(BEASTContext context, BEASTInterface alignment) {
+        MutableAlignmentOperator operator = new MutableAlignmentOperator();
+        operator.setInputValue("mutableAlignment", alignment);
+        operator.initAndValidate();
+        operator.setID("mutableAlignmentOperator");
+        context.addExtraOperator(operator);
+    }
+
+
 
     @Override
     public Class<ReadCountModel> getGeneratorClass() {
