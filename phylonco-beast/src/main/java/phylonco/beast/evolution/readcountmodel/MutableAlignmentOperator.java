d625fc908b1aa6982e44b3f0151820ed9c8336f6
zjzxiaohei <108013625+zjzxiaohei@users.noreply.github.com>
2025-03-04 23:03:38 +1300

    A alignment operator which can change the state in alignment #56

---
+++ phylonco-beast/src/main/java/phylonco/beast/evolution/readcountmodel/MutableAlignmentOperator.java
@@ -0,0 +1,46 @@
+package phylonco.beast.evolution.readcountmodel;
+
+import beast.base.core.Description;
+import beast.base.core.Input;
+import beast.base.inference.Operator;
+import mutablealignment.MutableAlignment;
+
+import java.util.Random;
+
+@Description("This operator changes an alignment.")
+public class MutableAlignmentOperator extends Operator {
+    public Input<MutableAlignment> mutableAlignmentInput = new Input<>("mutableAlignment", "mutableAlignment");
+    private Random random;
+    private MutableAlignment mutableAlignment;
+    private Integer site;
+    private Integer taxa;
+    private Integer oldState;
+    private Integer newState;
+
+    public MutableAlignmentOperator() {}
+
+
+    @Override
+    public void initAndValidate() {
+        mutableAlignment = mutableAlignmentInput.get();
+        random = new Random();
+    }
+
+
+    @Override
+    public double proposal() {
+        taxa = random.nextInt(mutableAlignment.getTaxonCount());
+        site = random.nextInt(mutableAlignment.getSiteCount());
+        oldState = mutableAlignment.getSiteValue(taxa, site);
+        do {
+            newState = mutableAlignment.getSiteValue(taxa, site);
+        } while (newState == oldState);
+        mutableAlignment.setSiteValue(taxa, site, newState);
+        return 0;
+    }
+
+
+
+
+
+}
