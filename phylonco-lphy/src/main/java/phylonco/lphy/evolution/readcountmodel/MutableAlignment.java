6352906d5bc4563855e926185d6a07d2271eb73a
Yao Xiao <xbarry1818@gmail.com>
2025-03-06 16:23:12 +1300

    mutable alignment in LPhy #56

---
+++ phylonco-lphy/src/main/java/phylonco/lphy/evolution/readcountmodel/MutableAlignment.java
@@ -0,0 +1,18 @@
+package phylonco.lphy.evolution.readcountmodel;
+
+
+
+import lphy.base.evolution.alignment.Alignment;
+import lphy.base.evolution.alignment.SimpleAlignment;
+
+
+
+public class MutableAlignment extends SimpleAlignment {
+
+    Alignment parent;
+
+    public MutableAlignment(int length, Alignment parent) {
+        super(length, parent); // copy source
+        this.parent = parent;
+    }
+}
