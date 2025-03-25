be5dfe06f7c73894d38acce9f99b634894912fbf
Yao Xiao <xbarry1818@gmail.com>
2025-03-25 15:20:12 +1300

    modify initAndValidate function #56

--- phylonco-beast/src/main/java/phylonco/beast/evolution/readcountmodel/MutableAlignmentOperator.java
+++ phylonco-beast/src/main/java/phylonco/beast/evolution/readcountmodel/MutableAlignmentOperator.java
@@ -8,36 +8,37 @@ import mutablealignment.MutableAlignment;
 
 @Description("This operator changes an alignment.")
 public class MutableAlignmentOperator extends Operator {
+
+
     public Input<MutableAlignment> mutableAlignmentInput = new Input<>("mutableAlignment", "mutableAlignment");
     private MutableAlignment mutableAlignment;
-    private Integer site;
-    private Integer taxa;
-    private Integer oldState;
-    private Integer newState;
+    private int numStates;
+    private int numSites;
+    private int numTaxa;
 
     public MutableAlignmentOperator() {}
-
 
     @Override
     public void initAndValidate() {
         mutableAlignment = mutableAlignmentInput.get();
+        numStates = mutableAlignment.getDataType().getStateCount();
+        numSites = mutableAlignment.getSiteCount();
+        numTaxa = mutableAlignment.getTaxonCount();
     }
 
 
     @Override
     public double proposal() {
-        taxa = Randomizer.nextInt(mutableAlignment.getTaxonCount());
-        site = Randomizer.nextInt(mutableAlignment.getSiteCount());
-        oldState = mutableAlignment.getSiteValue(taxa, site);
+        int taxa = Randomizer.nextInt(numTaxa);
+        int site = Randomizer.nextInt(numSites);
+        int oldState = mutableAlignment.getSiteValue(taxa, site);
+        int newState;
         do {
-            int numStates = mutableAlignment.getDataType().getStateCount();
             newState = Randomizer.nextInt(numStates);
-        } while (newState == oldState);
+        } while (newState==oldState);
         mutableAlignment.setSiteValue(taxa, site, newState);
         return 0;
     }
-
-
 
 
 
