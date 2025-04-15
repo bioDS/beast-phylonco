6165fdf2d2e0855c25214e4c7213f13effd618c8
EvaLiyt <liyutong0617@gmail.com>
2025-04-15 13:54:36 +1200

    clarify wildtype and alternative overdispersion #56

--- phylonco-lphy/src/main/java/phylonco/lphy/evolution/readcountmodel/ReadCountModel.java
+++ phylonco-lphy/src/main/java/phylonco/lphy/evolution/readcountmodel/ReadCountModel.java
@@ -1,6 +1,8 @@
 package phylonco.lphy.evolution.readcountmodel;
 
+import lphy.base.distribution.Categorical;
 import lphy.base.distribution.DirichletMultinomial;
+import lphy.base.distribution.UniformDiscrete;
 import lphy.base.evolution.Taxa;
 import lphy.base.evolution.alignment.Alignment;
 import lphy.core.model.GenerativeDistribution;
@@ -34,7 +36,8 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
 
     private Value<Integer2DMatrix> coverage;
     private Value<Alignment> data;
-    private Value<Double> w;
+    private Value<Number> w1;
+    private  Value<Number> w2;
     private Value<Double> epsilon;
     private RandomVariable<ReadCountData> readCountData;
     private Value<Integer2DMatrix> alpha;
@@ -44,7 +47,8 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
     public static final String covParamName = "coverage";
     public static final String alphaParamName = "alpha";
     public static final String epsilonParamName = "epsilon";
-    public static final String wParamName = "w";
+    public static final String w1ParamName = "w1";
+    public static final String w2ParamName = "w2";
 
 
     public ReadCountModel(
@@ -52,14 +56,16 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
             @ParameterInfo(name = covParamName, description = "coverage.") Value<Integer2DMatrix> coverage,
             @ParameterInfo(name = alphaParamName, description = "allelic dropout events for each cell at each site.") Value<Integer2DMatrix> alpha,
             @ParameterInfo(name = epsilonParamName, description = "sequencing and amplification error probability.") Value<Double> epsilon,
-            @ParameterInfo(name = wParamName, description = "overdispersion parameter of Dirichlet multinomial distribution.") Value<Double> w
+            @ParameterInfo(name = w1ParamName, description = "wildtype overdispersion parameter of Dirichlet multinomial distribution.") Value<Number> w1,
+            @ParameterInfo(name = w2ParamName, description = "alternative overdispersion parameter for Dirichlet multinomial distribution") Value<Number> w2
             ) {
         super();
         this.data = data;
         this.coverage = coverage;
         this.alpha = alpha;
         this.epsilon = epsilon;
-        this.w = w;
+        this.w1 = w1;
+        this.w2 = w2;
     }
 
     @GeneratorInfo(name = "ReadCountModel",
@@ -76,7 +82,6 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
         int l = data.value().nchar();
         Double eps = epsilon.value();
         Integer[][][] readC = new Integer[n][l][4];
-        dirichletMultinomial.setParam("w", this.w);
 
         Double[][] propensities = {
                 {(1 - eps), (eps/3), (eps/3), (eps/3)},             // AA or A_ 0
@@ -96,6 +101,18 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                 int stateIndex = data.value().getState(i, j);
                 PhasedGenotypeState genotypeState = PhasedGenotype.getCanonicalState(stateIndex);
                 String genotype = genotypeState.getFullName();//get genotype
+
+                // map w
+                // if 0/1 or 1/1', use w2
+                if (genotypeState.getIndex() > 21) {
+                    // if the state is unknown or gap, then sample one
+                    genotypeState = PhasedGenotype.getCanonicalState(sampleGenotype());
+                } else if (genotypeState.getIndex() > 15 && genotypeState.getIndex() < 22) {
+                    dirichletMultinomial.setParam("w", w2);
+                } else {
+                    // use w1 when 0/0, 0/-, 1/1, 1/-
+                    dirichletMultinomial.setParam("w", w1);
+                }
 //                Integer cov_ij = coverage.value()[i][j];
                 Integer cov_ij = coverage.value().getState(i,j);
                 Double[] prob;
@@ -104,10 +121,8 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                 if (genotype.equals("AA")){
                     prob = propensities[0];
                     readC[i][j] = DirichletMultinomialSample(prob);
-                }
 
-
-                else if (genotype.equals("AC") || genotype.equals("CA")){
+                } else if (genotype.equals("AC") || genotype.equals("CA")){
                     if (alpha.value().getState(i,j) == 1){
                         if (Math.random() < 0.5){
                             prob = propensities[0];
@@ -155,10 +170,8 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                 } else if (genotype.equals("CC")){
                     prob = propensities[4];
                     readC[i][j] = DirichletMultinomialSample(prob);
-                }
 
-
-                else if (genotype.equals("CG") || genotype.equals("GC")){
+                } else if (genotype.equals("CG") || genotype.equals("GC")){
                     if (alpha.value().getState(i,j) == 1){
                         if (Math.random() < 0.5){
                             prob = propensities[4];
@@ -171,7 +184,6 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                         prob = propensities[5];
                         readC[i][j] = DirichletMultinomialSample(prob);
                     }
-
 
                 } else if (genotype.equals("CT") || genotype.equals("TC")){
                     if (alpha.value().getState(i,j) == 1){
@@ -187,14 +199,11 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                         readC[i][j] = DirichletMultinomialSample(prob);
                     }
 
-
                 } else if (genotype.equals("GG")){
                     prob = propensities[7];
                     readC[i][j] = DirichletMultinomialSample(prob);
-                }
 
-
-                else if (genotype.equals("GT") || genotype.equals("TG")){
+                } else if (genotype.equals("GT") || genotype.equals("TG")){
                     if (alpha.value().getState(i,j) == 1){
                         if (Math.random() < 0.5){
                             prob = propensities[7];
@@ -207,7 +216,6 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                         prob = propensities[8];
                         readC[i][j] = DirichletMultinomialSample(prob);
                     }
-
 
                 } else if (genotype.equals("TT")){
                     prob = propensities[9];
@@ -239,6 +247,15 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
         return dirichletMultinomial.sample().value();
     }
 
+    private int sampleGenotype(){
+        Value<Integer> lower = new Value<>("lower", 0);
+        Value<Integer> upper = new Value<>("upper", 0);
+        UniformDiscrete uniformDiscrete = new UniformDiscrete(lower, upper);
+        RandomVariable<Integer> genotype = uniformDiscrete.sample();
+
+        return genotype.value();
+    }
+
     @Override
     public Map<String, Value> getParams() {
         return Map.of(
@@ -246,7 +263,8 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
                 covParamName, coverage,
                 alphaParamName, alpha,
                 epsilonParamName, epsilon,
-                wParamName, w
+                w1ParamName, w1,
+                w2ParamName, w2
         );
     }
 
@@ -256,7 +274,8 @@ public class ReadCountModel implements GenerativeDistribution<ReadCountData> {
         else if (paramName.equals(covParamName)) coverage = value;
         else if (paramName.equals(alphaParamName)) alpha = value;
         else if (paramName.equals(epsilonParamName)) epsilon = value;
-        else if (paramName.equals(wParamName)) w = value;
+        else if (paramName.equals(w1ParamName)) w1 = value;
+        else if (paramName.equals(w2ParamName)) w2 = value;
         else throw new RuntimeException("Unrecognised parameter name: " + paramName);
 
         //super.setParam(paramName, value); // constructDistribution
