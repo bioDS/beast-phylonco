842215206cd260b4facc328d15ab3eb665c0460a
Yao Xiao <xbarry1818@gmail.com>
2025-10-07 11:09:03 +1300

    added an optional param ref for reading ref nucleotide from .rc file #56

--- phylonco-lphy/src/main/java/phylonco/lphy/evolution/readcountmodel/ReadTaxaReadCountMatrix.java
+++ phylonco-lphy/src/main/java/phylonco/lphy/evolution/readcountmodel/ReadTaxaReadCountMatrix.java
@@ -13,7 +13,6 @@ import lphy.core.model.annotation.IOFunction;
 import lphy.core.model.annotation.ParameterInfo;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,21 +27,30 @@ import java.util.List;
 )
 public class ReadTaxaReadCountMatrix extends DeterministicFunction<ReadCountData> {
 
-    public ReadTaxaReadCountMatrix(@ParameterInfo(name = ReaderConst.FILE, description = "the name of read count matrix file including path, which contains an alignment.") Value<String> filePath) {
+    private Value<Boolean> ref;
+    private boolean ifReadReference;
+    private int[] refIndex;
+
+    public ReadTaxaReadCountMatrix(@ParameterInfo(name = ReaderConst.FILE, description = "the name of read count matrix file including path, which contains an alignment.") Value<String> filePath,
+                                   @ParameterInfo(name = "ref", description = "the optional param(true or false), whether to read reference nucleotide from .rc file. By default, ref is false", optional = true) Value<Boolean> ref
+                                   ) {
         if (filePath == null) throw new IllegalArgumentException("The file name can't be null!");
         setParam(ReaderConst.FILE, filePath);
+        if (ref != null) {
+                this.ref =ref;
+        }
 
     }
     @GeneratorInfo(name="readTaxaReadCountMatrix", verbClause = "is read from", narrativeName = "rc file",
             category = GeneratorCategory.TAXA_ALIGNMENT,
             description = "A function that parses an read count matrix from a rc file.")
     public Value<ReadCountData> apply() {
-
+        ifReadReference = ifReadRef();
         String filePath = ((Value<String>) getParams().get(ReaderConst.FILE)).value();
         Path nexPath = UserDir.getUserPath(filePath);
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(nexPath.toFile()));
+            BufferedReader reader = getReader(nexPath.toString());
             ReadCountData data = getReadCountData(reader);
             return new Value<>(null, data, this);
         } catch (IOException e) {
@@ -63,21 +71,53 @@ public class ReadTaxaReadCountMatrix extends DeterministicFunction<ReadCountData
         if (allLines.isEmpty()) {
             throw new IllegalArgumentException("File is empty or contains no valid data");
         }
-        int n = allLines.size();
-        int l = allLines.get(0).split("\t").length-1;
-        String[] cellNames = new String[n];
-        Integer[][][] readCounts = new Integer[n][l][4];
-        for (int i = 0; i < n; i++) {
-            String[] data = allLines.get(i).split("\t");
-            cellNames[i] = data[0];
-            for (int j = 0; j < data.length-1; j++) {
-                String[] readC = data[j+1].split(":");
-                if(readC.length > ReadCount.NUM_NUCLEOTIDES) {System.out.println("the number of read counts at position: "+j+" for cell: "+ data[0] +" is higher than 4");}
-                for (int k = 0; k < ReadCount.NUM_NUCLEOTIDES; k++) {
-                    readCounts[i][j][k] = Integer.parseInt(readC[k]);
+        Integer[][][] readCounts;
+        String[] cellNames;
+        int n;
+        int l;
+
+        if (ifReadReference) {
+            n = allLines.size()-1;
+            l = allLines.get(0).split("\t").length-1;
+            cellNames = new String[n];
+            readCounts = new Integer[n][l][4];
+            refIndex = new int[l];
+            for (int i = 0; i < l; i++) {
+                refIndex[i] = Integer.parseInt(allLines.get(0).split("\t")[i+1]);
+            }
+            for (int i = 0; i < n; i++) {
+                String[] data = allLines.get(i+1).split("\t");
+                cellNames[i] = data[0];
+                for (int j = 0; j < data.length-1; j++) {
+                    String[] readC = data[j+1].split(":");
+                    if(readC.length > ReadCount.NUM_NUCLEOTIDES) {
+                        throw new IllegalArgumentException("The length of read counts at position: "+j+" is not 4, but is " + readC.length);
+                    }
+                    for (int k = 0; k < ReadCount.NUM_NUCLEOTIDES; k++) {
+                        readCounts[i][j][k] = Integer.parseInt(readC[k]);
+                    }
+                }
+            }
+        } else {
+            n = allLines.size();
+            l = allLines.get(0).split("\t").length-1;
+            cellNames = new String[n];
+            readCounts = new Integer[n][l][4];
+            for (int i = 0; i < n; i++) {
+                String[] data = allLines.get(i).split("\t");
+                cellNames[i] = data[0];
+                for (int j = 0; j < data.length-1; j++) {
+                    String[] readC = data[j+1].split(":");
+                    if(readC.length != ReadCount.NUM_NUCLEOTIDES) {
+                        throw new IllegalArgumentException("The length of read counts at position: "+j+" is not 4, but is " + readC.length);
+                    }
+                    for (int k = 0; k < ReadCount.NUM_NUCLEOTIDES; k++) {
+                        readCounts[i][j][k] = Integer.parseInt(readC[k]);
+                    }
                 }
             }
         }
+
         Taxa taxa = Taxa.createTaxa(cellNames);
         ReadCount[][] readCountMatrix = new ReadCount[n][l];
         for (int i = 0; i < n; i++) {
@@ -95,16 +135,15 @@ public class ReadTaxaReadCountMatrix extends DeterministicFunction<ReadCountData
             sitesIndex[i] = i;
         }
         ReadCountData readCountData = new ReadCountData(taxa, readCountMatrix, sitesIndex);
+        if (ifReadReference) {
+            readCountData.setRefIndex(refIndex);
+        }
         return readCountData;
     }
 
     private BufferedReader getReader(String fileName) {
         BufferedReader reader = null;
         try {
-            // Common Extensions: .fasta, .fas, .fa
-            // Other Extensions (depending on sequence type):
-            // .fna (nucleotide sequences), .ffn (nucleotide sequences),
-            // .faa (amino acid sequences), .frn (RNA sequences), .mpfa (multiple protein FASTA)
             if (!(fileName.endsWith("rc")))
                 throw new IOException("Read count matrix file name's suffix is invalid ! " + fileName);
 
@@ -120,6 +159,12 @@ public class ReadTaxaReadCountMatrix extends DeterministicFunction<ReadCountData
             LoggerUtils.logStackTrace(e);
         }
         return reader;
+    }
+
+    private boolean ifReadRef(){
+        if (ref != null) {
+            return ref.value();
+        } else return false;
     }
 
 }
